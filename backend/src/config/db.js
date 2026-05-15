eebdb47fe69cd6fdad194c8bd05f40a4ca6a7f6f
Tarun Singodia <tarunmukeshsingodia@gmail.com>
2026-05-15 19:08:26 +0530

    chore: add deployment guide and update environment examples for Firebase/Vercel setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose =require("mongoose");
 
 
 function connectToDB() {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("SERVER IS CONNECTED TO DB");
     })
